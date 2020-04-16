1df904b799f35584f8575a8f9d6efea62b569f64
Francois Best <francois@francoisbest.com>
2020-04-16 19:02:08 +0200

    fix: Use sendBeacon for session:end

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,11 @@ function makeUrl(config: Config, perf: number, xhr: string) {
   return `${config.pushURL}?${query}`
 }
 
-export function sendEvent(event: Event<any, any>, config: Config) {
+export function sendEvent(
+  event: Event<any, any>,
+  config: Config,
+  preferSendBeacon = false
+) {
   const tick = performance.now()
   const json = JSON.stringify(event)
   const payload = encryptString(json, config.publicKey)
@@ -59,33 +63,58 @@ export function sendEvent(event: Event<any, any>, config: Config) {
     return false
   }
 
-  if ('fetch' in window) {
-    const url = makeUrl(config, perf, 'fetch')
-    fetch(url, {
-      method: 'POST',
-      body: payload,
-      credentials: 'omit',
-      cache: 'no-store',
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'text/plain'
-      }
-    })
-    return true
-  }
-
   const beaconUrl = makeUrl(config, perf, 'beacon')
+  const fetchUrl = makeUrl(config, perf, 'fetch')
+  const imageUrl = makeUrl(config, perf, 'img')
+
+  type Strategy = [(url: string, payload: string) => boolean, string]
+  const strategies: Strategy[] = preferSendBeacon
+    ? [
+        [sendViaBeacon, beaconUrl],
+        [sendViaFetch, fetchUrl],
+        [sendViaImageGet, imageUrl]
+      ]
+    : [
+        [sendViaFetch, fetchUrl],
+        [sendViaBeacon, beaconUrl],
+        [sendViaImageGet, imageUrl]
+      ]
+  for (const [send, url] of strategies) {
+    if (send(url, payload)) {
+      break
+    }
+  }
+  return true
+}
+
+function sendViaFetch(url: string, payload: string) {
+  if (!('fetch' in window)) {
+    return false
+  }
+  fetch(url, {
+    method: 'POST',
+    body: payload,
+    credentials: 'omit',
+    cache: 'no-store',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  })
+  return true
+}
+function sendViaBeacon(url: string, payload: string) {
   if (
     typeof navigator.sendBeacon === 'function' &&
-    navigator.sendBeacon(beaconUrl, payload)
+    navigator.sendBeacon(url, payload)
   ) {
     return true
   }
-
-  // Fallback to img GET
-  const imgUrl = makeUrl(config, perf, 'img')
+  return false
+}
+function sendViaImageGet(url: string, payload: string) {
   const img = new Image()
-  img.src = `${imgUrl}&payload=${payload}`
+  img.src = `${url}&payload=${payload}`
   return true
 }
 
@@ -105,7 +134,9 @@ export function setup() {
     // any session tracking or other visitor information.
     sendEvent(createBrowserEvent('session:dnt', null), config)
   } else {
-    setupSessionListeners(event => sendEvent(event, config))
+    setupSessionListeners((event, lowPriority = false) =>
+      sendEvent(event, config, lowPriority)
+    )
     setupPageVisitListeners(event => sendEvent(event, config))
   }
   window.chiffre.sendNumber = data => {
