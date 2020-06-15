234cecc4ea9dc8eb910c54b0658638b58d778a8b
Francois Best <francois@francoisbest.com>
2020-06-15 19:02:00 +0200

    fix: Define process.env.NODE_ENV
    It fixes a crash where process is unknown in
    the browser. Tree shaking is not yet implemented
    in esbuild, but this should be a start.
    
    Also ignore build.json in the Surge upload.

---
+++ scripts/build.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import { build, BuildOptions } from 'esbuild'
+
+const options: BuildOptions = {
+  entryPoints: [path.resolve(__dirname, '../src/main.ts')],
+  outfile: path.resolve(__dirname, '../dist/analytics.js'),
+  bundle: true,
+  minify: true,
+  format: 'iife',
+  external: ['crypto'],
+  logLevel: 'info',
+  metafile: path.resolve(__dirname, '../dist/build.json'),
+  define: {
+    'process.env.NODE_ENV': process.env.NODE_ENV || 'production'
+  }
+}
+
+build(options).catch(() => process.exit(1))
