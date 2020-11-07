be6ebfd5cc815166a7ccf568ca7d3cb00518c5e3
Francois Best <francois@francoisbest.com>
2020-11-07 09:06:32 +0100

    chore: Update git ref file name

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,7 +5,7 @@ const readPkg = require('read-pkg')
 function readSha1(libDir) {
   try {
     return fs
-      .readFileSync(path.resolve(libDir, 'git-sha1.txt'))
+      .readFileSync(path.resolve(libDir, '.gitref'))
       .toString('utf-8')
       .trim()
   } catch {
@@ -16,12 +16,13 @@ function readSha1(libDir) {
 readPkg().then(pkg => {
   const libDir = path.resolve(__dirname, '../lib')
   const gitSha1 = readSha1(libDir)
-  const version = pkg.version + (gitSha1 ? '-' + gitSha1 : '')
+  const version = pkg.version + (gitSha1 ? '-' + gitSha1.slice(0, 8) : '')
   const ts = `export declare const version = "${version}";`
   const js = `"use strict";
   Object.defineProperty(exports, "__esModule", { value: true });
   exports.version = void 0;
   exports.version = '${version}';
+  // Full Git SHA-1: ${gitSha1}
 `
   fs.writeFileSync(path.resolve(libDir, 'version.js'), js)
   fs.writeFileSync(path.resolve(libDir, 'version.d.ts'), ts)
